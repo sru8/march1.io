056d000d02fff3a61f1fdbdbdf39e592b1fa2d06
sru8 <65797261+sru8@users.noreply.github.com>
2022-03-06 18:01:54 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,19 @@
+
+var ed = " teacher info" ;
+
+var new_obj = {
+    'first_name': ""
+}
+function clicks() {
+    prompt("enter num")
+}
+function click2() {
+    for(i=0;i<30;i++)
+    {
+        if(i%3==0)
+        document.write("fun");
+        else if (i%5==0)
+        document.write("double fun");
+        else document.write(i);
+    }
+}
