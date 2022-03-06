31e70118e24e7d3440dec59075ef149d9843da19
sru8 <65797261+sru8@users.noreply.github.com>
2022-03-06 17:57:07 +0530

    Add files via upload

---
+++ addition.js
@@ -0,0 +1,13 @@
+import './addition.css';
+
+function addition {
+    var a1 = getElementBy(a1, String)
+    var a2 = getElementBy(a2, String)
+}
+addition()
+
+
+function sum {
+    a1 + a2 = sum
+    console.write(sum)
+}
